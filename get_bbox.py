efd9ae05183b510884df0b245fbb09a8f816573f
Dan Tasse <dan.tasse@gmail.com>
2016-01-22 15:53:57 -0800

    whoop a lot of changes

---
+++ get_bbox.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Scrapes a whole city from Flickr.
+
+import argparse, csv, collections, ConfigParser, tqdm, flickr_api
+from jinja2 import Environment, PackageLoader, Template
+from shapely import geometry
+import util
+parser = argparse.ArgumentParser()
+parser.add_argument('--neighborhoods_file', default='data/sffind_neighborhoods.json')
+parser.add_argument('--target_neighborhood', default='Noe Valley')
+args = parser.parse_args()
+
+if __name__ == '__main__': 
+    nghds = util.load_neighborhoods(args.neighborhoods_file)
+    # photo_id, date, date_taken_unknown, lon, lat, tags
+
+    for nghd in nghds:
+        if nghd['properties']['name'].lower() == args.target_neighborhood.lower():
+            nghd_of_interest = nghd
+            nghd_geom = geometry.asShape(nghd_of_interest['geometry'])
+            bbox = str(nghd_geom.bounds)[1:-1]
+            print nghd['properties']['name']
+            print bbox
+ 
+
+
