b56cd1399fab85211ffef9ef3598e2bf69649d59
Dan Tasse <dan.tasse@gmail.com>
2016-02-10 15:49:52 -0800

    get bounding box for a whole city

--- get_bbox.py
+++ get_bbox.py
@@ -10,13 +10,20 @@ import util
 parser = argparse.ArgumentParser()
 parser.add_argument('--neighborhoods_file', default='data/sffind_neighborhoods.json')
 parser.add_argument('--target_neighborhood', default='Noe Valley')
+parser.add_argument('--whole_city', action='store_true')
 args = parser.parse_args()
 
 if __name__ == '__main__': 
     nghds = util.load_neighborhoods(args.neighborhoods_file)
     # photo_id, date, date_taken_unknown, lon, lat, tags
 
+    if args.whole_city:
+        city_geom = geometry.asShape(nghds[0]['geometry'])
+        
     for nghd in nghds:
+        if args.whole_city:
+            city_geom = city_geom.union(geometry.asShape(nghd['geometry']))
+
         if nghd['properties']['name'].lower() == args.target_neighborhood.lower():
             nghd_of_interest = nghd
             nghd_geom = geometry.asShape(nghd_of_interest['geometry'])
@@ -25,4 +32,7 @@ if __name__ == '__main__':
             print bbox
  
 
-
+    if args.whole_city:
+        city_bbox = str([round(b, 6) for b in city_geom.bounds])[1:-1]
+        print "Whole city bounding box:"
+        print city_bbox
